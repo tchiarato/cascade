fc4caf7715823a83fc46a3de3550ae40d9dcc22d
Thiago Dutra Chiarato <thiago.chiarato@hotmail.com>
2014-07-25 09:50:33 -0300

    Create jquery cascade helper

---
+++ jquery.cascadeSelect.js
@@ -0,0 +1,54 @@
+﻿(function (window, undefined) {
+
+    var $currentSelect, self;
+
+    function Cascade() {
+
+        self = this;
+
+        this.init = function () {
+            $('select[data-cascade]').each(function (index, el) {
+                var $el = $(el);
+                var master = $(el).data('master');
+                var url = $(el).data('action');
+
+                $el.attr('disabled', true);
+
+                $(document).on('change', '#' + master, function () {
+                    var value = $(':selected', this).val();
+                    $currentSelect = $el;
+
+                    if (value && value !== undefined && value !== "" && value !== 0)
+                        $.get(url, { id: value }, self.done);
+                    else
+                        self.reset($(this));
+                });
+            });
+        }
+
+        this.reset = function ($el) {
+            var $master = $el || $currentSelect;
+            var $select = $('select[data-master="' + $master.prop('id') + '"]');
+
+            $select.empty();
+            $select.attr('disabled', true);
+
+            $select.trigger('change');
+        }
+    }
+
+    Cascade.prototype.done = function (res) {
+
+        $currentSelect.empty();
+        res.forEach(Cascade.prototype.assemble);
+        self.reset();
+    }
+
+    Cascade.prototype.assemble = function (element, index) {
+
+        $currentSelect.append('<option value="' + element.Value + '">' + element.Text + '</option>');
+        $currentSelect.removeAttr('disabled');
+    }
+
+    window.Cascade = Cascade;
+})(window);
